b4543fbd45f3681448ae797ef98492b5af1bd55b
SebastianHeiberg <seba3673@stud.kea.dk>
2023-08-23 09:58:55 +0200

    a working prototype

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './componenets/Imageviewer';
+
+const PlaceholderImage = require('./assets/images/it-factory.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View><Text style={styles.textFrontPage}>The safest way to make a good investment</Text></View>      
       <StatusBar style="auto" />
+      <View><Button 
+      title='Invest your money now'
+      onPress={ () => alert ("... and the money is gone ")}
+      />
+      </View>
     </View>
+    
   );
 }
 
@@ -16,5 +28,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textFrontPage: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 20,
+    color: 'blue',
   },
 });
